f67a88241f4fafb2f58c32aada4237547319e1d8
XuanQuang203 <nguyenxuanquang.tmu.it@gmail.com>
2024-06-23 21:45:02 +0700

    Update Quan_ly_giao_dich.cpp

--- Quan_ly_giao_dich.cpp
+++ Quan_ly_giao_dich.cpp
@@ -1,89 +1,123 @@
 #include "Quan_ly_thong_tin_phong.cpp"
-#pragma once
 
 class Check_in 
 {
-	protected:
-		struct Thoi_gian
-		{
-			int Ngay_vao, Gio_vao;
-			
-			Thoi_gian() {}
-			
-			Thoi_gian(int _Ngay_vao, int _Gio_vao)
-			: Ngay_vao(_Ngay_vao), Gio_vao(_Gio_vao) {}
-		} tg_ci;
-	    
 	public:
+		int Gio_vao;
+		
 		void Nhap_thoi_gian_vao() 
+		{   
+	        cout << "Gio check-in: ";
+	        cin >> Gio_vao;
+	    }
+	
+	    int get_Gio_vao()
 		{
-	        cout << "Nhap ngay check-in: ";
-	        cin >> tg_ci.Ngay_vao;
-	        
-	        cout << "Nhap gio check-in: ";
-	        cin >> tg_ci.Gio_vao;
+	        return Gio_vao;
+	    }
+	
+	    void set_Gio_vao(int _Gio_vao) 
+		{
+	        Gio_vao = _Gio_vao;
 	    }
 	    
-	    int get_Ngay_vao()
-		{
-	        return tg_ci.Ngay_vao;
-	    }
+	    void Xoa_ci()
+	    {
+	    	Gio_vao = 0;
+		}
 	
-	    void set_Ngay_vao(int Ngay_vao) 
+	    void Xuat_thoi_gian_vao()
 		{
-	        Ngay_vao = Ngay_vao;
-	    }
-	
-	    int get_Gio_vao() 
-		{
-	        return tg_ci.Gio_vao;
-	    }
-	
-	    void set_Gio_vao(int Gio_vao) 
-		{
-	        Gio_vao = Gio_vao;
-	    }
-	
-	    void Xuat_thoi_gian_vao() 
-		{
-	        cout << "Ngay check-in: " << tg_ci.Ngay_vao << endl;
-	        cout << "Gio check-in: " << tg_ci.Gio_vao << endl;
+	        cout << "Gio check-in: " << Gio_vao << endl;
 	    }
 	    
 	    friend class Check_out;
-	    friend class Thanh_toan;
 };
 
-class Check_out : public Check_in 
+class Check_out : public Check_in
 {
 	protected:
+		struct Ngay_thang_nam 
+		{
+	        int Ngay, Thang, Nam;
+	
+	        Ngay_thang_nam() {}
+	
+	        Ngay_thang_nam(int _Ngay, int _Thang, int _Nam)
+	            : Ngay(_Ngay), Thang(_Thang), Nam(_Nam) {}
+	    } NTN;
+		
 		struct Thoi_gian
 		{
-			int Ngay_ra, Gio_ra;
+			Ngay_thang_nam Ngay_ra;
+			int Gio_ra;
 			
 			Thoi_gian() {}
 			
-			Thoi_gian(int _Ngay_ra, int _Gio_ra)
+			Thoi_gian(Ngay_thang_nam _Ngay_ra, int _Gio_ra)
 			: Ngay_ra(_Ngay_ra), Gio_ra(_Gio_ra) {}
 		} tg_co;
 		
 	public:
+		bool kt_yy(int yy) {
+		    return yy >= 2024;
+		}
+		
+		bool yy_nhuan(int yy) {
+		    return (yy % 4 == 0 && yy % 100 != 0) || (yy % 400 == 0);
+		}
+		
+		int kt_mm(int mm, int yy) {
+		    switch (mm) {
+		        case 1: return 31;
+		        case 2: return yy_nhuan(yy) ? 29 : 28;
+		        case 3: return 31;
+		        case 4: return 30;
+		        case 5: return 31;
+		        case 6: return 30;
+		        case 7: return 31;
+		        case 8: return 31;
+		        case 9: return 30;
+		        case 10: return 31;
+		        case 11: return 30;
+		        case 12: return 31;
+		        default: return 0;
+		    }
+		}
+		
+		bool kt_dd(int dd, int mm, int yy) {
+		    int maxDay = kt_mm(mm, yy);
+		    return dd >= 1 && dd <= maxDay;
+		}
+		
 		void Nhap_thoi_gian_ra() 
 		{
-	        cout << "Nhap ngay check-out: ";
-	        cin >> tg_co.Ngay_ra;
-	        cout << "Nhap gio check-out: ";
+			cout << "Gio check-out: ";
 	        cin >> tg_co.Gio_ra;
+	        
+	        do 
+				{
+					cout << "Ngay check-out (dd mm yyyy): "; 
+		        	cin >> NTN.Ngay >> NTN.Thang >> NTN.Nam;
+		        	
+		            if (!kt_yy(NTN.Nam) || !kt_mm(NTN.Thang, NTN.Nam) || !kt_dd(NTN.Ngay, NTN.Thang, NTN.Nam)) 
+					{
+		                cout << "Khong hop le, vui long nhap lai!\n";
+		                cout << "---------------------\n";
+		            }
+		        } 
+				while (!kt_yy(NTN.Nam) || !kt_mm(NTN.Thang, NTN.Nam) || !kt_dd(NTN.Ngay, NTN.Thang, NTN.Nam));
+		        tg_co.Ngay_ra = NTN;
 	    }
 		
 	    int get_Ngay_ra() 
 		{
-	        return tg_co.Ngay_ra;
+	        return NTN.Ngay;
 	    }
 	
 	    void set_Ngay_ra(int Ngay_ra) 
 		{
-	        tg_co.Ngay_ra = Ngay_ra;
+	        NTN.Ngay = Ngay_ra;
 	    }
 	
 	    int get_Gio_ra() 
@@ -95,88 +129,19 @@ class Check_out : public Check_in
 		{
 	        tg_co.Gio_ra = Gio_ra;
 	    }
+	    
+	    void Xoa_co()
+	    {
+	    	tg_co.Gio_ra = 0;
+	    	NTN.Ngay = 0;
+	    	NTN.Thang = 0;
+	    	NTN.Nam = 0;
+	    	tg_co.Ngay_ra = NTN;
+		}
 	
 	    void Xuat_thoi_gian_ra()
 		{
-	        cout << "Ngay check-out: " << tg_co.Ngay_ra << endl;
+	        cout << "Ngay check-out: " << tg_co.Ngay_ra.Ngay << "/" << tg_co.Ngay_ra.Thang << "/" << tg_co.Ngay_ra.Nam << endl;
 	        cout << "Gio check-out: " << tg_co.Gio_ra << endl;
 	    }
-	    
-	    friend class Thanh_toan;
 };
-
-class Thanh_toan : public Check_out
-{
-	private:
-	    int Thanh_tien, Gia_ngay = 2500000, Gia_gio = 150000;
-	    int So_ngay_thue, So_gio_thue;
-	    
-	public:
-		int Lap_hoa_don() 
-		{
-			int tien, x, y;
-	        x = Check_out::get_Ngay_ra() - Check_in::get_Ngay_vao();
-	        if(x == 0)
-			{
-	        	y = (Check_out::get_Gio_ra() - Check_in::get_Gio_vao());
-			}
-			else if(x > 0)
-			{
-				y = (Check_out::get_Ngay_ra() - Check_in::get_Ngay_vao()) * 24 + (Check_out::get_Gio_ra() - Check_in::get_Gio_vao());
-			}
-			else
-			{
-				cout << "Loi tinh toan, vui long kiem tra lai thong tin da nhap!\n";
-			}
-			
-			if(y == 0)
-			{
-	        	cout << "Loi tinh toan, vui long kiem tra lai thong tin da nhap!\n";
-			}
-			return y;
-			
-			tien = x * Gia_ngay + y * Gia_gio;
-
-	        cout << "HOA DON\n";
-	        cout << "---------------------\n";
-	        cout << "Thong tin thanh toan: \n";
-	        if(x == 0)
-			{
-				cout << "Tong so gio thue: " << y << endl;
-			}
-			else if(x > 0)
-			{
-				cout << "So ngay thue: " << y << endl;
-				cout << "Tong so gio thue: " << y << endl;
-			}
-	        
-	        
-	        cout << "\nThanh tien: " << Thanh_tien << " vnd" << endl;
-	        
-	        int luachon;
-	        cout << "---------------------\n";
-	        cout << "Phuong thuc thanh toan:\n";
-	        cout << "01. Tien mat\n";
-	        cout << "02. Chuyen khoan\n";
-	        cout << "Khach chon: ";
-	        chonlai:
-	        cin >> luachon;
-	        
-	        if(luachon == 1)
-			{
-	        	cout << "XAC NHAN PHUONG THUC: Tien mat";
-			}
-			else if(luachon == 2)
-			{
-				cout << "XAC NHAN PHUONG THUC: Chuyen khoan";
-			}
-			else
-			{
-				cout << "Loi nhap, vui long thu lai!";
-				goto chonlai;
-			}
-			cout << "---------------------\n";
-			cout << "Cam on quy khach da su dung dich vu!";
-	    }
-};
-
